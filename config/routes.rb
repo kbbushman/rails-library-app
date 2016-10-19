6016c9afbf843592d19006926400c3a791811f2a
kbbushman <k.bushman@yahoo.com>
2016-10-19 15:20:14 -0700

    adds VCM for user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "users#index"
+
+  get '/signup', to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
+
+  post '/sessions', to: 'sessions#create'
+
+  resources :users
+
 end
